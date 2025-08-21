1e09391af70288b7691ffbac03091f957e8a9c2f
Jack V <jack93.vab@gmail.com>
2025-08-21 21:49:36 +0300

    end get Average

--- JS/GetGrade.js
+++ JS/GetGrade.js
@@ -1,19 +1,40 @@
+function getAverage(scores) {
+  let sum = 0
+
+  for (const score of scores) {
+    sum += score
+  }
+
+  return sum / scores.length
+}
+
 function getGrade(score) {
   if (score === 100) {
     return 'A++'
-  } else if (score <= 99 && score >= 90) {
+  } else if (score >= 90) {
     return 'A'
-  } else if (score <= 89 && score >= 80) {
+  } else if (score >= 80) {
     return 'B'
-  } else if (score <= 79 && score >= 70) {
+  } else if (score >= 70) {
     return 'C'
-  } else if (score <= 69 && score >= 60) {
+  } else if (score >= 60) {
     return 'D'
   } else {
     return 'F'
   }
 }
 
-console.log(getGrade(96))
-console.log(getGrade(82))
-console.log(getGrade(56))
+function hasPassingGrade(score) {
+  return getGrade(score) !== 'F'
+}
+
+function studentMsg(totalScores, studentScore) {
+  return `Class average: ${getAverage(totalScores)}. Your grade: ${getGrade(
+    studentScore
+  )}. ${
+    hasPassingGrade(studentScore)
+      ? 'You passed the course.'
+      : 'You failed the course.'
+  }`
+}
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37))
